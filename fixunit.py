c29b25dcf6ed6cf4df461fd321c8cd36d293b07a
Stanislav Malyshev <smalyshev@gmail.com>
2017-06-26 12:26:07 -0700

    Update Units scripts

--- fixunit.py
+++ fixunit.py
@@ -7,24 +7,34 @@ This scripts fixes wrong units in quantities.
 Two forms:
 cat id-list | python3 fixunit.py PROP TRUE-UNIT
 or:
-python3 fixunit.py PROP TRUE-UNIT FALSE-UNIT
+python3 fixunit.py PROP TRUE-UNIT FALSE-UNIT [TYPE]
 """
 
 if len(sys.argv) < 3:
-	print(HELP)
-	exit()
+    print(HELP)
+    exit()
 
 PROP = sys.argv[1]
 UNIT = sys.argv[2]
+if len(sys.argv) > 4:
+    TYPE = sys.argv[4]
+else:
+    TYPE = None
 QUERY = """
 SELECT ?id WHERE {
-    ?id p:%s/psv:%s [ wikibase:quantityUnit wd:%s ]
+    ?id p:%s/psv:%s [ wikibase:quantityUnit wd:%s ] .
+	%s
     FILTER(?id != wd:Q4115189 && ?id != wd:Q13406268 && ?id != wd:Q15397819)
 }
 """
 STDUNITS = {
     '1': 'Q199',
+	'bad1': '1',
     '%': 'Q11229',
+    'cm': 'Q174728',
+    'km': 'Q828224',
+    'kg': 'Q11570',
+    'kmh': 'Q180154',
 }
 
 sparql_query = SparqlQuery()
@@ -36,8 +46,15 @@ else:
         UNIT = STDUNITS[UNIT]
     UNIT = 'http://www.wikidata.org/entity/' + UNIT
 
+if TYPE:
+	QTYPE = "?id wdt:P31 wd:%s ." % TYPE
+else:
+	QTYPE = ""
+
+
 site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
+site.throttle.setDelays(writedelay=1)
 
 if len(sys.argv) <= 3:
     source = sys.stdin
@@ -46,10 +63,14 @@ else:
     bad_unit = sys.argv[3]
     if bad_unit in STDUNITS:
         bad_unit = STDUNITS[bad_unit]
-    sparql = QUERY % (PROP, PROP, bad_unit)
+    sparql = QUERY % (PROP, PROP, bad_unit, QTYPE)
+    print(sparql)
     items = sparql_query.get_items(sparql, item_name="id")
     print("%s items found: %s" % (len(items), items))
     source = items
+
+if len(source) > 600:
+	print("Too many items, you are probably doing something wrong. Let's re-think it.")
 
 for qid in source:
     qid = qid.strip()
@@ -71,6 +92,7 @@ for qid in source:
         if UNIT == None and target.unit == '1':
             continue
         if target.unit != UNIT:
-            print("Bad unit for %s:%s - want %s, now %s. Fixing." % (qid, PROP, UNIT, target.unit))
+            comment = "Bad unit for %s:%s - want %s, now %s. Fixing." % (qid, PROP, UNIT, target.unit)
+            print(comment)
             target._unit = UNIT
-            claim.changeTarget(target)
+            claim.changeTarget(target, summary=comment)
--- units.py
+++ units.py
@@ -2,12 +2,6 @@ import pywikibot
 from pywikibot.data.sparql import SparqlQuery
 from urllib.parse import quote
 
-"""
-This script scans values with units and identifies those 
-that have both unitless (Q199) and united values.
-Results posted to LOGPAGE.
-"""
-
 QUANT = """
 SELECT ?p WHERE {
   ?p a wikibase:Property; wikibase:propertyType wikibase:Quantity .
@@ -20,8 +14,9 @@ SELECT ?unit (count(?x) as ?count) WHERE {
 ORDER BY DESC(?count)
 """
 GETUNITS = """
-SELECT ?id ?idLabel WHERE {
-    ?id p:%s/psv:%s [ wikibase:quantityUnit wd:%s ]
+SELECT ?id ?idLabel ?type ?typeLabel WHERE {
+    ?id p:%s/psv:%s [ wikibase:quantityUnit wd:%s ] .
+    OPTIONAL { ?id wdt:P31 ?type }
     FILTER(?id != wd:Q4115189 && ?id != wd:Q13406268 && ?id != wd:Q15397819)
     SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
 }
@@ -37,6 +32,7 @@ site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
 
 badprops = []
+counts = {}
 items = sparql_query.get_items(QUANT, item_name='p')
 
 sandboxes = set(['Q13406268', 'Q15397819', 'Q4115189'])
@@ -45,19 +41,20 @@ sandboxes = set(['Q13406268', 'Q15397819', 'Q4115189'])
 # total produced (P1092)
 # quantity (P1114)
 # numeric value (P1181)
-# collection or exhibition size (P1436) 
+# collection or exhibition size (P1436)
 # personal best (P2415)
 # number of parts of a work of art (P2635)
 # number of participants (P1132)
-mixed_units = set(['P1083', 'P1092', 'P1181', 'P1114', 'P1436', 'P2415', 'P2635', 'P1132'])
+mixed_units = set(['P1083', 'P1092', 'P1181', 'P1114', 'P1132', 'P1436', 'P2415', 'P2635' ])
 # Properties with known anomalous units
 allowed_anomaly = {
     'P1110': set(['http://www.wikidata.org/entity/Q6256', 'http://www.wikidata.org/entity/Q7275']),
+    'P1113': set(['http://www.wikidata.org/entity/Q1261214']),
+    'P1122': set(['http://www.wikidata.org/entity/Q12503', 'http://www.wikidata.org/entity/Q743139']),
     'P1971': set(['http://www.wikidata.org/entity/Q177232', 'http://www.wikidata.org/entity/Q308194']),
     'P2103': set(['http://www.wikidata.org/entity/Q28997']),
     'P2124': set(['http://www.wikidata.org/entity/Q37226', 'http://www.wikidata.org/entity/Q43229', 'http://www.wikidata.org/entity/Q515']),
     'P2196': set(['http://www.wikidata.org/entity/Q21094885']),
-    'P1122': set(['http://www.wikidata.org/entity/Q12503', 'http://www.wikidata.org/entity/Q743139'])
 }
 
 # report inconsistent properties
@@ -86,6 +83,8 @@ def found_inconsistent(prop, result):
             "|| {{Q|" + unitName + "}} || " + \
             unit['count'] + "||" + \
             SPARQL % quote(query) + "\n"
+        if unitName == 'Q199':
+            counts[prop] = unit['count']
     text = text + "|}\n"
     text = text + "[http://query.wikidata.org/#%s Try again]\n" % quote(CHECKUNITS % (prop, prop))
     logpage.text = text
@@ -107,5 +106,5 @@ for item in items:
 #
 if badprops:
     logpage = pywikibot.Page(site, LOGPAGE)
-    logpage.text = "\n\n".join([ "{{P|" + prop + "}} [[" + LOGPAGE+"/"+prop + "]]" for prop in sorted(badprops) ])
+    logpage.text = "\n\n".join([ "{{P|" + prop + "}} [[" + LOGPAGE+"/"+prop + "]] (" + counts[prop] + ")" for prop in sorted(badprops) ])
     logpage.save("log for units")
